0d7964c19b7e0710af6af0927710bf92a01c559a
Shouting Lyu <lyu.s@husky.neu.edu>
2018-07-04 12:28:27 -0400

    adding mongo

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('./config/keys');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 const app = express();
 //https://accounts.google.com/o/oauth2/v2/auth?
 // response_type=code&
 // redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth%2Fgoogle%2Fcallback&
-
-passport.use(
-    new GoogleStrategy(
-        {
-            clientID: keys.googleClientID,
-            clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback'
-        },(accessToken)=>{
-            console.log(accessToken);
-        })
-);
-
-app.get(
-    '/auth/google',
-    passport.authenticate('google',{
-        scope: ['profile', 'email']
-    })
-);
-
+require('./routes/authRoutes')(app);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
