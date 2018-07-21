8ffee9008fdfef50cc0e5700ab59ae21638c9451
Shouting Lyu <lyu.s@husky.neu.edu>
2018-07-21 22:23:57 +0800

    development and production

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '718611316721-jgfgf0n5bm3foud1dhk2p1igr366tf4r.apps.googleusercontent.com',
+    googleClientSecret: 'TkdurKG74DFOyaD5G3EKrFxO',
+    mongoURI: 'mongodb://shoutinglyu:846140499lst@ds127771.mlab.com:27771/shouting-feedbackapp-dev',
+    cookieKey: 'xcnbvjakbeufidkjansdjaksveuehknadkfnsdkc'
+};
